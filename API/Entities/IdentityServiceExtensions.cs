12bc6dfb2e46c6773278db0e0e7986b59c7e71c2
Anderson Miyake <afmyk@hotmail.com>
2024-02-25 19:43:43 -0300

    End of section 4

---
+++ API/Entities/IdentityServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+         IConfiguration config)
+         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    //all of the rules about how our server will validate de token
+
+                    //check the token signing key
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding
+                        .UTF8.GetBytes(config["TokenKey"])),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+         }
+}
